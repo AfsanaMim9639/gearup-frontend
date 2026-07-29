238b25301d3221517dc571bac28e3d0848e8b29a
Afsana Mim <afsanamim9639@gmail.com>
2026-07-29 06:21:32 +0600

    chore: initial Next.js setup with Shadcn UI and custom dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+  lang="en"
+  className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
+>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
